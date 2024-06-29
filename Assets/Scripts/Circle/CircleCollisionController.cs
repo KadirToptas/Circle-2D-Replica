1bcca59abc814f01facd0252287f6202a368c2e7
KadirToptas <kdrrtptss@gmail.com>
2024-06-29 16:58:13 +0300

    Circle-2D-Replica

--- Assets/Scripts/Circle/CircleCollisionController.cs
+++ Assets/Scripts/Circle/CircleCollisionController.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CircleCollisionController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
         
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Line"))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
